f912943854dc4903e139608e5aabad99aa7435be
sreehari20050 <sreehari22sreehari@gmail.com>
2024-10-28 18:24:50 +0530

    Update Move Zero's to End.cpp

--- Array/Move Zero's to End.cpp
+++ Array/Move Zero's to End.cpp
@@ -1,4 +1,5 @@
- vector<int> b;
+//solution 1
+vector<int> b;
     int zero = 0;
     for(int i = 0 ; i < n ; i++){
         if(a[i] != 0){b.push_back(a[i]);}
@@ -11,3 +12,27 @@
     }
 
     return b;
+
+
+//solution 2
+
+int j = -1;
+    //place the pointer j:
+    for (int i = 0; i < n; i++) {
+        if (a[i] == 0) {
+            j = i;
+            break;
+        }
+    }
+    //no non-zero elements:
+    if (j == -1) return a;
+
+    //Move the pointers i and j
+    //and swap accordingly:
+    for (int i = j + 1; i < n; i++) {
+        if (a[i] != 0) {
+            swap(a[i], a[j]);
+            j++;
+        }
+    }
+    return a;
