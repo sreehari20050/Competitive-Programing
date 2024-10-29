406eaad1a5ebc4e99f50aed75132f826c7256c57
sreehari20050 <sreehari22sreehari@gmail.com>
2024-10-29 13:06:49 +0530

    Update Merge 2 Sorted Array.cpp

--- Array/Merge 2 Sorted Array.cpp
+++ Array/Merge 2 Sorted Array.cpp
@@ -17,3 +17,45 @@ vector < int > sortedArray(vector < int > a, vector < int > b) {
      return Union;
 }
 
+
+
+
+
+//Solution 2
+vector < int > sortedArray(vector < int > a, vector < int > b) {
+    // Write your code here
+    int i= 0;
+    int j = 0;
+    int n1 = a.size();
+    int n2 = b.size();
+    vector<int> Union;
+
+    while(i<n1 && j<n2){
+      if(a[i] <= b[j]){
+        if (Union.size() == 0 || Union.back() != a[i]) {
+          Union.push_back(a[i]);
+        }
+        i++;
+      } else {
+        if (Union.size() == 0 || Union.back() != b[j]) {
+          Union.push_back(b[j]);
+        }
+          j++;
+      }
+    }
+    while(i<n1){
+      if(Union.size() == 0 || Union.back() != a[i]){
+          Union.push_back(a[i]);
+        }
+        i++;
+    }
+    while(j<n2){
+        if (Union.size() == 0 || Union.back() != b[j]) {
+          Union.push_back(b[j]);
+        }
+        j++;
+    }
+    
+    return Union;
+
+}
