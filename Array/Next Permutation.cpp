5b7d21b8d2c7f82df44a3f37b85045b003d2df3b
Sreehari K B <sreehari22sreehari@gmail.com>
2024-11-02 14:49:21 +0530

    Create Next Permutation.cpp

---
+++ Array/Next Permutation.cpp
@@ -0,0 +1,13 @@
+//Solution 1
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+        
+    }
+};
+
+
+
+//Solution 2
